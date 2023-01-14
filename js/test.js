96cadbd869e5ec3bb1f637298e681468d97e5dad
zabieru363 <zabierujlc@gmail.com>
2023-01-14 16:35:11 +0100

    Creando objetos con la factoria hasta Resource

--- js/test.js
+++ js/test.js
@@ -1,50 +1,47 @@
 "use strict";
 
-import * as Entities from "./entities.js";
+import ObjectFactory from "./entities.js";
 
 (function() {
+
+    // ? Creando un objeto Fxtory para testear los objetos planos.
+    const factory = new ObjectFactory();
+
     // * PROBANDO A CREAR INSTANCIAS DE LAS ENTIDADES DEL SISTEMA.
 
     // ? Objeto Person
     console.log("Objeto Person");
     
-    const person = new Entities.Person("Javier", "López", "Carretero", new Date(2000, 8, 6), 'C:\\Users\\images');
+    const person = factory.createPerson("Javier", "López", "Carretero", new Date(2000, 8, 6), 'C:\\Users\\images');
     console.log(person);    // {}
     console.log(person.name);   // Javier
     
     // ! EXCEPCIONES DEL OBJETO PERSON.
     
-    // Invocar al constructor sin new:
-    try {
-        const person = Entities.Person("Javier", "López", "Carretero", new Date(2000, 8, 6), 'C:\\Users\\images');
-    }catch(error) {
-        console.error(error);   // El constructor no puede ser invocado sin new.
-    }
-    
     // El nombre está sin definir:
     try {
-        const person = new Entities.Person("", "López", "Carretero", new Date(2000, 8, 6), 'C:\\Users\\images');
+        const person = factory.createPerson("", "López", "Carretero", new Date(2000, 8, 6), 'C:\\Users\\images');
     }catch(error) {
         console.error(error);   // El campo name no puede estar vacío.
     }
     
     // El primer apellido está sin definir:
     try {
-        const person = new Entities.Person("Javier", "", "Carretero", new Date(2000, 8, 6), 'C:\\Users\\images');
+        const person = factory.createPerson("Javier", "", "Carretero", new Date(2000, 8, 6), 'C:\\Users\\images');
     }catch(error) {
         console.error(error);   // El campo lastName1 no puede estar vacío.
     }
     
     // La fecha de nacimiento está sin definir:
     try {
-        const person = new Entities.Person("Javier", "López", "Carretero", "", 'C:\\Users\\images');
+        const person = factory.createPerson("Javier", "López", "Carretero", "", 'C:\\Users\\images');
     }catch(error) {
         console.error(error);   // El campo born no puede estar vacío.
     }
     
     // El campo born no es una fecha:
     try {
-        const person = new Entities.Person("Javier", "López", "Carretero", "2000, 8, 6", 'C:\\Users\\images');
+        const person = factory.createPerson("Javier", "López", "Carretero", "2000, 8, 6", 'C:\\Users\\images');
     }catch(error) {
         console.error(error);   // EL objeto que se está pasando no es una fecha.
     }
@@ -52,22 +49,15 @@ import * as Entities from "./entities.js";
     // ? Objeto Category
     console.log("Objeto Category");
     
-    const category = new Entities.Category("Videojuegos", "Hablando sobre videojuegos.");
+    const category = factory.createCategory("Videojuegos", "Hablando sobre videojuegos.");
     console.log(category);  // {}
     console.log(category.name);     //Videojuegos
     
     // ! EXCEPCIONES DEL OBJETO CATEGORY.
     
-    // Invocar al constructor sin new:
-    try {
-        const category = Entities.Category("Videojuegos", "Hablando sobre videojuegos.");
-    }catch(error) {
-        console.error(error);   // El constructor no puede ser invocado sin new.
-    }
-    
     // El nombre de la categoría está sin definir.
     try {
-        const category = new Entities.Category("", "Hablando sobre videojuegos.");
+        const category = factory.createCategory("", "Hablando sobre videojuegos.");
     }catch(error) {
         console.error(error);   // El campo name no puede estar vacio.
     }
@@ -75,21 +65,14 @@ import * as Entities from "./entities.js";
     // ? Objeto Resource
     console.log("Objeto Resource");
 
-    const resource = new Entities.Resource(120, "videosystem.com\\recurso1");
+    const resource = factory.createResource(120, "videosystem.com\\recurso1");
     console.log(resource);  // {}
     
     // ! EXCEPCIONES DEL OBJETO RESOURCE.
-    
-    // Invocar al constructor sin new:
-    try {
-        const resource = Entities.Resource(120, "videosystem.com\\recurso1");
-    }catch(error) {
-        console.error(error);   // El constructor no puede ser invocado sin new.
-    }
 
     // La duración en minutos está sin definir:
     try {
-        const resource = new Entities.Resource("", "videosystem.com\\recurso1");
+        const resource = factory.createResource("", "videosystem.com\\recurso1");
     }catch(error) {
         console.error(error);   // El campo duration no está definido.
     }
