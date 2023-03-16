ad74ef8974e752b61da85b0624b02c57cff696e3
zabieru363 <zabierujlc@gmail.com>
2023-03-16 13:02:43 +0100

    Cambio hecho en el php

---
+++ php/post.php
@@ -0,0 +1,15 @@
+<?php
+    date_default_timezone_set("Europe/Madrid");
+    
+    $now = getdate();
+    
+    echo "Fecha actual:" . "<br>";
+    echo "Dia: " . $now["mday"] . " Mes: " . $now["mon"] . " Año: " . $now["year"];
+    
+    echo " Hora actual";
+    echo $now["hours"] . ":" . $now["minutes"] . ":" . $now["seconds"];
+    
+    echo "La copia de seguridad se realizó correctamente";
+    $data = json_decode($_POST["backup"]);
+    echo $_POST["backup"];
+?>
